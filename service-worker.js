a1acc03a5aab9f2cdaea3f9c376e848884c67d04
raushansync <raushanguptaicloud@gmail.com>
2026-05-12 02:14:00 +0530

    color fixed

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 ﻿/* RaushanSYNC Learning Platform PWA Service Worker */
-const CACHE_VERSION = 'app-v1.1.2.4.3';  // Generic version for cross-site use
+const CACHE_VERSION = 'app-v1.1.2.4.4';  // Generic version for cross-site use
 const CORE_CACHE = 'rs-core-' + CACHE_VERSION;
 const RUNTIME_CACHE = 'rs-runtime-' + CACHE_VERSION;
 const OFFLINE_URL = '/offline/';
